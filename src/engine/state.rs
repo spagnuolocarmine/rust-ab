3d5e222b91bb973dee08b4bb5c641ba9d3ebf77f
zioposty <l.postiglione4@studenti.unisa.it>
2021-06-30 13:56:12 +0200

    trait state update

--- src/engine/state.rs
+++ src/engine/state.rs
@@ -2,8 +2,11 @@ use crate::engine::agent::Agent;
 use crate::engine::schedule::Schedule;
 use std::any::Any;
 pub trait State {
+    fn reset(&mut self);
     fn update(&mut self, step: usize) {}
     fn init(&mut self, schedule: &mut Schedule);
     fn as_any(&mut self) -> &mut dyn Any;
-    fn as_state(&mut self) -> &mut dyn State; 
+    fn as_state(&mut self) -> &mut dyn State;
+    fn before_step(&mut self, schedule: &mut Schedule);
+    fn after_step(&mut self, schedule: &mut Schedule);
 }
